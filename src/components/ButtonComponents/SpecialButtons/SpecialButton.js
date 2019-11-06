0f7029422977771dd4f3d68d00db0a51959e02d9
astubbings <53932400+astubbings@users.noreply.github.com>
2019-11-05 17:18:42 -0700

    pull req setup commit

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = props => {
   return (
-    <>
+    <button className="specials_button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
